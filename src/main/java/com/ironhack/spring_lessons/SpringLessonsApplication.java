16d490682fd9eeb3d321a79aff5edcdc4d80d7cd
Adrián Maza <adri_mv1@hotmail.com>
2025-05-13 12:51:59 +0200

    w15d2

--- src/main/java/com/ironhack/spring_lessons/SpringLessonsApplication.java
+++ src/main/java/com/ironhack/spring_lessons/SpringLessonsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLessonsApplication {
+//	W15D2
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLessonsApplication.class, args);
